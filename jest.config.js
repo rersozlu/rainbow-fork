425f07339a047c7ad5e4dc7ec6340211d803ffd2
estrattonbailey <git@esb.lol>
2022-10-05 14:42:29 -0500

    fix: remove ts-jest from unit testing jest config

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,6 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['./config/test/jest-setup.js'],
   testPathIgnorePatterns: ['node_modules', 'e2e'],
-  transform: {
-    '\\.tsx?$': 'ts-jest',
-  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|react-native-keyboard-area|@react-native(-community)?)/)',
   ],
